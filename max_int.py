a99042159c7d2d42cf1cf193c02bb1d662849915
Stefan Bjarni Hjaltested <stefanh@Stefans-MacBook-Pro.local>
2018-09-04 11:39:11 +0000

    laga folder struture

--- max_int.py
+++ max_int.py
@@ -1,11 +1,14 @@
-# Design an algorithm that finds the maximum positive integer input by a user.  The user repeatedly inputs numbers until a negative value is entered.
- 
-# Make sure that you write up the algorithm before starting to code.
-# Then implement the algorithm in Python. Put your algorithmic description as a comment in the program file.
+# User inputs integer
+# while integer is positive the user enters another integer
+# when user enters integer that is negative the program prints the highest number.
 
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
-while num_int > 0:
-    int(input("Input a number: "))
-
-print("The maximum is", max_int)    # Do not change this line
+max_int = 0
+while num_int >= 0:
+    num_int = int(input("Input a number: "))
+    if num_int > max_int:
+        max_int = num_int
+else:
+    print("The maximum is", max_int)    # Do not change this line
+#comment frá bjögga
