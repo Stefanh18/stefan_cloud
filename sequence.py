5026c6f1c4ef40412d8776f44a809ca2528dc510
Stefan Bjarni Hjaltested <stefanh@Stefans-MacBook-Pro.local>
2018-09-04 12:01:19 +0000

    loka

--- sequence.py
+++ sequence.py
@@ -3,3 +3,12 @@
 # the program prints out the sequence 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+sum_of_int = 0
+counter = 0
+for x in range(1, n+1):
+    if x <= 3:
+        sum_of_int = x
+    else:
+        sum_of_int = sum_of_int * 2 - counter
+        counter += 1
+    print(sum_of_int)
