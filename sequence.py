b405362250582762fa3480e142c612906682fe3c
Stefan Bjarni Hjaltested <stefanh@Stefans-MacBook-Pro.local>
2018-09-04 11:48:06 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,5 @@
+# The program generates prints out the sum of previos 3 numbers starting at number 1
+# If there are less than 3 number before the output it prints out the sum of them
+# the program prints out the sequence 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
